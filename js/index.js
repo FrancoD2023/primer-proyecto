06e32f0b00835fd6f3c041e718a5577afcf163b1
FrancoD2023 <125236085+FrancoD2023@users.noreply.github.com>
2023-03-11 18:28:53 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const root = document.getElementById('root')
+
+root.innerHTML = `
+    <h2 class="titulo">Hola Mundo!</h2>
+    <p style="font-weight:bold;">Realizado desde Github CodeSpaces</p>
+`
